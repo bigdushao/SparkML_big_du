93057446c89391fb8332702dbb8c56a31b6a1339
dushao <1414163982@qq.com>
2018-01-24 10:31:56 +0800

    图片

--- src/main/java/org/du/yarn/Util.java
+++ src/main/java/org/du/yarn/Util.java
@@ -28,6 +28,7 @@ import org.w3c.dom.NodeList;
  * Created by dushao on 18-1-22.
  */
 public class Util {
+
     private static final char[] HEX_CHARS = new char[] { '0', '1', '2', '3',
             '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 
